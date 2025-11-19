d79ecac63f1070eb0abcf9f8998358e6c2b4cdf1
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2025-11-19 14:34:52 +0900

    feat: add C# WebAPI Implementation

---
+++ CSharp/Api/Program.cs
@@ -0,0 +1,48 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapHealthChecks("/healthz");
+app.MapGet("/", () => MachineInformation.Default);
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            WeatherForecast.Summaries[Random.Shared.Next(WeatherForecast.Summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public static readonly string[] Summaries = [ "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal record MachineInformation(string MachineName, string OSDescription, int ProcessorCount, long TotalMemoryInMB)
+{
+    public static MachineInformation Default { get; } = new MachineInformation(
+        Environment.MachineName,
+        System.Runtime.InteropServices.RuntimeInformation.OSDescription,
+        Environment.ProcessorCount,
+        GC.GetGCMemoryInfo().TotalAvailableMemoryBytes / (1024 * 1024)
+    );
+}
